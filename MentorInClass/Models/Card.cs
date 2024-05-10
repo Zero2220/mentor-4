ae0223fe4f16035e03b0a509c46fd5e6e58fe207
anarpi <anarpi@code.edu.az>
2024-05-10 18:28:09 +0400

    new

---
+++ MentorInClass/Models/Card.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MentorInClass.Models
+{
+    public class Card
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string BtnContext { get; set; }
+        public bool IsFeatured { get; set; }
+        public bool IsAdvanced { get; set; }
+        public List<CardFeature>? CardFeatures { get; set; }
+        [NotMapped]
+        public List<int>? FeatureIds { get; set; } = new List<int>();
+    }
+}
